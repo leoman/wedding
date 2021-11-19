4a6a35035d3eadcaf1472c794a4d471369321598
Peter Mansell <peter@pmansell.com>
2021-11-19 21:43:51 +0000

    ipad

--- src/components/Parallax/styles.js
+++ src/components/Parallax/styles.js
@@ -19,6 +19,15 @@ export const ParallaxWrapper = styled.section`
     @media only screen 
     and (min-device-width: 768px) 
     and (max-device-width: 1024px) 
+    and (-webkit-min-device-pixel-ratio: 1) {
+        background-size: contain;
+        background-position: center center !important;
+        background-attachment: local;
+    }
+
+    @media only screen 
+    and (min-device-width: 768px) 
+    and (max-device-width: 1024px) 
     and (-webkit-min-device-pixel-ratio: 2) {
         background-size: contain;
         background-position: center center !important;
